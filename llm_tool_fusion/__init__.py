537ec50d2f8544eebafb6f266652e7919f7b0f03
caua1503 <cauamedinax@gmail.com>
2025-05-24 12:15:24 -0300

    update

--- llm_tool_fusion/__init__.py
+++ llm_tool_fusion/__init__.py
@@ -3,4 +3,4 @@ from ._utils import process_tool_calls, process_tool_calls_async
 
 __all__ = ["ToolCaller", "process_tool_calls", "process_tool_calls_async"]
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
